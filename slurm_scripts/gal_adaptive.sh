eb828897caa25c34ce6e8379e9606568fcee7612
Alan Yang <ayang1097@gmail.com>
2024-09-26 15:18:11 -0700

    adds experiment scripts + results

---
+++ slurm_scripts/gal_adaptive.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+############################## Submit Job ######################################
+#SBATCH --time=24:00:00
+#SBATCH --job-name="gal_adaptive"
+#SBATCH --mail-user=yalan@stanford.edu
+#SBATCH --mail-type=END
+#SBATCH --output=gal_adaptive_%j.txt
+#SBATCH --error=gal_adaptive_%j.txt
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 32
+#SBATCH --mem=4G
+#SBATCH --partition=normal
+#####################################
+
+# run using sbatch --array=0,10,20,30,40,50,1000 gps_l1.sh
+
+# Load module for Gurobi and Julia (should be most up-to-date version, i.e. 1.7.2)
+module load python/3.9
+
+# Change to the directory of script
+export SLURM_SUBMIT_DIR=/scratch/users/yalan/decor
+
+# Change to the job directory
+cd $SLURM_SUBMIT_DIR
+
+python3 -m scripts.optimize --seed $SLURM_ARRAY_TASK_ID --n 100 --len 4092 --p 6 --iters 1_000_000_000 --method AdaptiveKGreedyCodeOptimizer
---
+++ slurm_scripts/l1c_m=1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+############################## Submit Job ######################################
+#SBATCH --time=24:00:00
+#SBATCH --job-name="l1c_m=1"
+#SBATCH --mail-user=yalan@stanford.edu
+#SBATCH --mail-type=END
+#SBATCH --output=l1c_m=1_%j.txt
+#SBATCH --error=l1c_m=1_%j.txt
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 128
+#SBATCH --mem=256G
+#SBATCH --partition=bigmem
+#####################################
+
+# run using sbatch --array=0,10,20,30,40,50,1000 gps_l1.sh
+
+# Load module for Gurobi and Julia (should be most up-to-date version, i.e. 1.7.2)
+module load python/3.9
+
+# Change to the directory of script
+export SLURM_SUBMIT_DIR=/scratch/users/yalan/decor
+
+# Change to the job directory
+cd $SLURM_SUBMIT_DIR
+
+python3 -m scripts.optimize --seed $SLURM_ARRAY_TASK_ID --n 420 --len 10230 --p 6 --iters 1_000_000_000 --method TopKGreedyCodeOptimizer --args num_neighbors=1
